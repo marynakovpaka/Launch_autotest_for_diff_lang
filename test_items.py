88c3fcdd302abb769a8ebd903fd535989751b4be
marynakovpaka <79002481+marynakovpaka@users.noreply.github.com>
2021-08-17 14:50:07 +0300

    Initial commit

---
+++ test_items.py
@@ -0,0 +1,7 @@
+
+def test_add_to_basket_button_is_present(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    assert browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket"), "Add to basket button is absent"
+
+
